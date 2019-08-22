0c561f67bee9987df359c3831f9c9ca8db691409
Piotr Łukomiak <cieplak32@gmail.com>
2019-08-22 20:55:09 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const process = require("process");
+const postcss = require("postcss");
+const { parse: parseValue } = require("postcss-values-parser");
+
+module.exports = async function parse(input) {
+    const customPropertyRegistry = {};
+
+    const result = await postcss().process(input);
+    result.root.walkDecls(decl => {
+        if (isCustomProperty(decl)) {
+            const { prop, value } = decl;
+            const parsed = parseValue(value);
+            customPropertyRegistry[prop] = getValue(parsed, customPropertyRegistry);
+        }
+    });
+
+    return customPropertyRegistry;
+};
+
+function isCustomProperty(node) {
+    return node.prop.startsWith("--");
+}
+
+function getValue(root, customPropertyRegistry) {
+    const node = root.nodes[0];
+    if (node.type == "word") {
+        return node.value;
+    } else if (node.type === "func") {
+        return compileFunc(node, customPropertyRegistry);
+    }
+
+    throw new Error(`Unsupported node type: "${node.type}"`);
+}
+
+function compileFunc(func, customPropertyRegistry) {
+    if (func.name !== "var") {
+        throw new Error(`Unsupported function "${func.name}"`);
+    }
+
+    const args = func.nodes;
+    const customPropertyName = args[0].value;
+    if (customPropertyRegistry.hasOwnProperty(customPropertyName)) {
+        return customPropertyRegistry[customPropertyName];
+    }
+
+    const fallback = args[2];
+    if (!fallback) {
+        return undefined;
+    }
+
+    return fallback.value;
+}
+
+function isEmpty(a) {
+    return a.length === 0;
+}
---
+++ test/index.js
@@ -0,0 +1,83 @@
+const test = require("tape");
+const parse = require("../");
+
+test("empty", async t => {
+    const input = ``;
+    const result = await parse(input);
+    t.deepEqual(result, {});
+    t.end();
+});
+
+test("empty selector", async t => {
+    const input = `body {}`;
+    const result = await parse(input);
+    t.deepEqual(result, {});
+    t.end();
+});
+
+test("simple variables", async t => {
+    const input = `
+        :root {
+            --var-1: #beeeef;
+            --var-2: red;
+        }
+    `;
+    const result = await parse(input);
+    t.deepEqual(result, { "--var-1": "#beeeef", "--var-2": "red" });
+    t.end();
+});
+
+test("multiple selectors", async t => {
+    const input = `
+        :root {
+            --var-1: #beeeef;
+        }
+        body {
+            --var-2: red;
+        }
+    `;
+    const result = await parse(input);
+    t.deepEqual(result, { "--var-1": "#beeeef", "--var-2": "red" });
+    t.end();
+});
+
+test("alias", async t => {
+    const input = `
+        :root {
+            --var-1: #beeeef;
+            --var-2: var(--var-1);
+        }
+    `;
+    const result = await parse(input);
+    t.deepEqual(result, { "--var-1": "#beeeef", "--var-2": "#beeeef" });
+    t.end();
+});
+
+test("alias with fallback", async t => {
+    const input = `
+        :root {
+            --var-1: #beeeef;
+            --var-2: var(--var-0, red);
+        }
+    `;
+    const result = await parse(input);
+    t.deepEqual(result, { "--var-1": "#beeeef", "--var-2": "red" });
+    t.end();
+});
+
+test("fails with any function other than var", async t => {
+    t.plan(1);
+
+    const input = `
+        :root {
+            --var-1: calc(100% - 16px);
+        }
+    `;
+
+    try {
+        await parse(input);
+        t.fail();
+    } catch (err) {
+        t.pass();
+    }
+});
