6a4a09d875c3eb84b60e64954aa94188bf376d78
Piotr Łukomiak <piotr.lukomiak@gmail.com>
2024-05-04 15:11:42 +0200

    Reformat

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,69 +1,69 @@
 import parse from "../lib";
 
 test("empty", async () => {
-    const input = ``;
-    const result = await parse(input);
-    expect(result).toStrictEqual({});
+	const input = ``;
+	const result = await parse(input);
+	expect(result).toStrictEqual({});
 });
 
 test("empty selector", async () => {
-    const input = `body {}`;
-    const result = await parse(input);
-    expect(result).toStrictEqual({});
+	const input = `body {}`;
+	const result = await parse(input);
+	expect(result).toStrictEqual({});
 });
 
 test("simple variables", async () => {
-    const input = `
+	const input = `
         :root {
             --var-1: #beeeef;
             --var-2: red;
         }
     `;
-    const result = await parse(input);
-    expect(result).toStrictEqual({ "--var-1": "#beeeef", "--var-2": "red" });
+	const result = await parse(input);
+	expect(result).toStrictEqual({ "--var-1": "#beeeef", "--var-2": "red" });
 });
 
 test("multiline variable value", async () => {
-    const input = `
+	const input = `
         :root {
             --var-1: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica,
                 Arial, sans-serif, Apple Color Emoji, Segoe UI Emojibody;
         }
     `;
-    const result = await parse(input);
-    expect(result).toStrictEqual({
-        "--var-1":
-            "-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emojibody",
-    });
+	const result = await parse(input);
+	expect(result).toStrictEqual({
+		"--var-1":
+			"-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emojibody",
+	});
 });
 
 test("text values in quotes", async () => {
-    const input = `
+	const input = `
         :root {
             --var-1: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica,
                 Arial, sans-serif, "Apple Color Emoji", 'Segoe UI Emojibody';
         }
     `;
-    const result = await parse(input);
-    expect(result).toStrictEqual({
-        "--var-1": `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emojibody"`,
-    });
+	const result = await parse(input);
+	expect(result).toStrictEqual({
+		"--var-1": `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emojibody"`,
+	});
 });
 
 test("numeric values", async () => {
-    const input = `
+	const input = `
         :root {
             --var-1: 500;
         }
     `;
-    const result = await parse(input);
-    expect(result).toStrictEqual({
-        "--var-1": 500,
-    });
+	const result = await parse(input);
+	expect(result).toStrictEqual({
+		"--var-1": 500,
+	});
 });
 
 test("unit values", async () => {
-    const input = `
+	const input = `
         :root {
             --var-1: 12px;
             --var-2: 18pt;
@@ -71,17 +71,17 @@ test("unit values", async () => {
             --var-4: 20;
         }
     `;
-    const result = await parse(input);
-    expect(result).toStrictEqual({
-        "--var-1": "12px",
-        "--var-2": "18pt",
-        "--var-3": "50vh",
-        "--var-4": 20,
-    });
+	const result = await parse(input);
+	expect(result).toStrictEqual({
+		"--var-1": "12px",
+		"--var-2": "18pt",
+		"--var-3": "50vh",
+		"--var-4": 20,
+	});
 });
 
 test("multiple selectors", async () => {
-    const input = `
+	const input = `
         :root {
             --var-1: #beeeef;
         }
@@ -89,57 +89,57 @@ test("multiple selectors", async () => {
             --var-2: red;
         }
     `;
-    const result = await parse(input);
-    expect(result).toStrictEqual({ "--var-1": "#beeeef", "--var-2": "red" });
+	const result = await parse(input);
+	expect(result).toStrictEqual({ "--var-1": "#beeeef", "--var-2": "red" });
 });
 
 test("alias", async () => {
-    const input = `
+	const input = `
         :root {
             --var-1: #beeeef;
             --var-2: var(--var-1);
         }
     `;
-    const result = await parse(input);
-    expect(result).toStrictEqual({ "--var-1": "#beeeef", "--var-2": "#beeeef" });
+	const result = await parse(input);
+	expect(result).toStrictEqual({ "--var-1": "#beeeef", "--var-2": "#beeeef" });
 });
 
 test("alias with fallback", async () => {
-    const input = `
+	const input = `
         :root {
             --var-1: #beeeef;
             --var-2: var(--var-0, red);
         }
     `;
-    const result = await parse(input);
-    expect(result).toStrictEqual({ "--var-1": "#beeeef", "--var-2": "red" });
+	const result = await parse(input);
+	expect(result).toStrictEqual({ "--var-1": "#beeeef", "--var-2": "red" });
 });
 
 test("alias non-existent variable without fallback", async () => {
-    const input = `
+	const input = `
         :root {
             --var-1: var(--color-primary);
         }
     `;
-    const result = await parse(input);
-    expect(result).toStrictEqual({ "--var-1": undefined });
+	const result = await parse(input);
+	expect(result).toStrictEqual({ "--var-1": undefined });
 });
 
 test("pass-through functions other than var", async () => {
-    const input = `
+	const input = `
         :root {
             --var-1: calc(100% - 16px);
         }
     `;
 
-    const result = await parse(input);
-    expect(result).toEqual({
-        "--var-1": "calc(100% - 16px)",
-    });
+	const result = await parse(input);
+	expect(result).toEqual({
+		"--var-1": "calc(100% - 16px)",
+	});
 });
 
 test("handles scss code", async () => {
-    const input = `
+	const input = `
         :root {
             @for $i from 1 through 4 {
                 --var-#{$i}: #{$i};
@@ -147,11 +147,11 @@ test("handles scss code", async () => {
         }
     `;
 
-    const result = await parse(input);
-    expect(result).toStrictEqual({
-        "--var-1": 1,
-        "--var-2": 2,
-        "--var-3": 3,
-        "--var-4": 4,
-    });
+	const result = await parse(input);
+	expect(result).toStrictEqual({
+		"--var-1": 1,
+		"--var-2": 2,
+		"--var-3": 3,
+		"--var-4": 4,
+	});
 });
--- lib/index.ts
+++ lib/index.ts
@@ -8,87 +8,87 @@ type PropertyRegistry = Map<string, PropertyValue>;
 type Result = Record<string, PropertyValue>;
 
 export default async function parse(input: string): Promise<Result> {
-    const customPropertyRegistry = new Map<string, PropertyValue>();
+	const customPropertyRegistry = new Map<string, PropertyValue>();
 
-    const { css } = sass.renderSync({ data: input });
-    const result = await postcss().process(css.toString());
-    result.root?.walkDecls((decl) => {
-        if (isCustomProperty(decl)) {
-            const { prop, value } = decl;
-            const parsed = parseValue(value);
-            customPropertyRegistry.set(prop, getValue(parsed, customPropertyRegistry));
-        }
-    });
+	const { css } = sass.renderSync({ data: input });
+	const result = await postcss().process(css.toString());
+	result.root?.walkDecls((decl) => {
+		if (isCustomProperty(decl)) {
+			const { prop, value } = decl;
+			const parsed = parseValue(value);
+			customPropertyRegistry.set(prop, getValue(parsed, customPropertyRegistry));
+		}
+	});
 
-    return mapToRecord(customPropertyRegistry);
+	return mapToRecord(customPropertyRegistry);
 }
 
 function isCustomProperty(node: Declaration): boolean {
-    return node.prop.startsWith("--");
+	return node.prop.startsWith("--");
 }
 
 function getValue(root: Root, customPropertyRegistry: PropertyRegistry): PropertyValue {
-    if (root.nodes.length === 1) {
-        const node = root.nodes[0];
-        if (node.type == "word" || node.type === "quoted") {
-            return node.value;
-        } else if (node.type === "numeric") {
-            return parseNumericValue(node);
-        } else if (node.type === "func") {
-            return compileFunc(node, customPropertyRegistry);
-        }
-        throw new Error(`Unsupported node type: "${node.type}"`);
-    } else if (root.nodes.length > 1) {
-        return collectNodes(root.nodes);
-    }
+	if (root.nodes.length === 1) {
+		const node = root.nodes[0];
+		if (node.type == "word" || node.type === "quoted") {
+			return node.value;
+		} else if (node.type === "numeric") {
+			return parseNumericValue(node);
+		} else if (node.type === "func") {
+			return compileFunc(node, customPropertyRegistry);
+		}
+		throw new Error(`Unsupported node type: "${node.type}"`);
+	} else if (root.nodes.length > 1) {
+		return collectNodes(root.nodes);
+	}
 
-    return undefined;
+	return undefined;
 }
 
 function collectNodes(nodes: ChildNode[]): string {
-    return nodes
-        .reduce<string[]>((acc, node) => {
-            if (node.type === "word" || node.type === "quoted") {
-                return isEmpty(acc) ? [...acc, node.value] : [...acc, " ", node.value];
-            } else if (node.type === "punctuation") {
-                return [...acc, ","];
-            }
+	return nodes
+		.reduce<string[]>((acc, node) => {
+			if (node.type === "word" || node.type === "quoted") {
+				return isEmpty(acc) ? [...acc, node.value] : [...acc, " ", node.value];
+			} else if (node.type === "punctuation") {
+				return [...acc, ","];
+			}
 
-            // else ignore
-            return acc;
-        }, [])
-        .join("");
+			// else ignore
+			return acc;
+		}, [])
+		.join("");
 }
 
 function parseNumericValue(node: Numeric): PropertyValue {
-    return node.unit ? `${node.value}${node.unit}` : +node.value;
+	return node.unit ? `${node.value}${node.unit}` : +node.value;
 }
 
 function compileFunc(func: Func, customPropertyRegistry: PropertyRegistry): PropertyValue {
-    if (func.name !== "var") {
-        return func.toString();
-    }
+	if (func.name !== "var") {
+		return func.toString();
+	}
 
-    const args = func.nodes;
-    if ("value" in args[0]) {
-        const customPropertyName = args[0].value;
-        if (customPropertyRegistry.has(customPropertyName)) {
-            return customPropertyRegistry.get(customPropertyName)!;
-        }
-    }
+	const args = func.nodes;
+	if ("value" in args[0]) {
+		const customPropertyName = args[0].value;
+		if (customPropertyRegistry.has(customPropertyName)) {
+			return customPropertyRegistry.get(customPropertyName)!;
+		}
+	}
 
-    const fallback = args[2];
-    if (!fallback || !("value" in fallback)) {
-        return undefined;
-    }
+	const fallback = args[2];
+	if (!fallback || !("value" in fallback)) {
+		return undefined;
+	}
 
-    return fallback.value;
+	return fallback.value;
 }
 
 function isEmpty(a: ArrayLike<any>): boolean {
-    return a.length === 0;
+	return a.length === 0;
 }
 
 function mapToRecord<V>(map: Map<PropertyKey, V>): Record<PropertyKey, V> {
-    return Object.fromEntries(map.entries());
+	return Object.fromEntries(map.entries());
 }
